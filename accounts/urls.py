f0df999522353b04abde8e0e559ffe54f9becbab
kyungsoo Jung <wjdrudtn97@naver.com>
2024-02-24 22:04:57 +0900

    password 변경

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.login, name='logout'),
     path('signup/', views.signup, name='signup'),   
     path('edit/', views.profile_edit, name='profile_edit'),
+    path('password_change/', views.password_change, name='password_change'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, redirect
-from .forms import SignupForm, ProfileForm
+from .forms import SignupForm, ProfileForm, PasswordChangeForm
 from django.contrib import messages
-from django.contrib.auth.views import LoginView, logout_then_login
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import(
+    LoginView, logout_then_login, 
+    PasswordChangeView as AuthPasswordChangeView,
+    ) 
+from django.urls import reverse_lazy
 from django.contrib.auth import login as auth_login
 from django.contrib.auth.decorators import login_required 
 # Create your views here.
@@ -43,3 +48,14 @@ def profile_edit(request):
         "form": form,
     })
     
+
+class PasswordChangeView(LoginRequiredMixin, AuthPasswordChangeView):
+    success_url = reverse_lazy("password_change")
+    template_name = 'accounts/password_change_form.html'
+    form_class = PasswordChangeForm
+
+    def form_valid(self, form):
+        messages.success(self.request, "암호를 변경했습니다.")
+        return super().form_valid(form)
+
+password_change = PasswordChangeView.as_view()
