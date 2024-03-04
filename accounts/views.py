0598f6c2142a1baef3ff3738812efe684b54aefa
kyungsoo Jung <wjdrudtn97@naver.com>
2024-03-04 23:26:23 +0900

    follow - unfollow 관계필드 구현 , follow 기능 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import SignupForm, ProfileForm, PasswordChangeForm
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -9,6 +9,7 @@ from django.contrib.auth.views import(
 from django.urls import reverse_lazy
 from django.contrib.auth import login as auth_login
 from django.contrib.auth.decorators import login_required 
+from .models import User
 # Create your views here.
 
 login = LoginView.as_view(template_name="accounts/login_form.html")
@@ -59,3 +60,21 @@ class PasswordChangeView(LoginRequiredMixin, AuthPasswordChangeView):
         return super().form_valid(form)
 
 password_change = PasswordChangeView.as_view()
+
+@login_required
+def user_follow(request, username):
+    follow_user = get_object_or_404(User, username=username, is_active=True)
+    request.user.following_set.add(follow_user)
+    follow_user.follower_set.add(request.user)
+    messages.success(request, f"{follow_user}님을 팔로우 했습니다.")
+    redirect_url = request.META.get("HTTP_REFERER", "root")
+    return redirect(redirect_url)
+
+@login_required
+def user_unfollow(request, username):
+    unfollow_user = get_object_or_404(User, username=username, is_active=True)
+    request.user.following_set.remove(unfollow_user)
+    unfollow_user.follower_set.remove(request.user)
+    messages.success(request, f"{unfollow_user}님을 언팔로우 했습니다.")
+    redirect_url = request.META.get("HTTP_REFERER", "root")
+    return redirect(redirect_url)
